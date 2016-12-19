62bbe82a3fc4f4b013d6a3a2637d49b029f27cc1
mmonsterzld <monster.z@Mac-mini.local>
2016-12-19 11:26:55 +0800

    111
    111

---
+++ 11212/11212/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  11212
+//
+//  Created by 戴良忠 on 2016/12/19.
+//  Copyright © 2016年 戴良忠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
